7e30521b01dd062cec0302c910ab3d4dc3526570
Daniil <porvipop@mail.ru>
2022-05-05 21:32:24 +0300

    Setup NetworkModule

---
+++ app/src/main/java/com/example/googleauthapp/di/NetworkModule.kt
@@ -0,0 +1,51 @@
+package com.example.googleauthapp.di
+
+import com.example.googleauthapp.util.Constants.BASE_URL
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.serialization.ExperimentalSerializationApi
+import kotlinx.serialization.json.Json
+import okhttp3.JavaNetCookieJar
+import okhttp3.MediaType
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import java.net.CookieManager
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object NetworkModule {
+
+    @Provides
+    @Singleton
+    fun provideCookieManager(): CookieManager {
+        return CookieManager()
+    }
+
+    @Provides
+    @Singleton
+    fun provideHttpClient(cookieManager: CookieManager): OkHttpClient {
+        return OkHttpClient.Builder()
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .readTimeout(15, TimeUnit.SECONDS)
+            .cookieJar(JavaNetCookieJar(cookieManager))
+            .build()
+
+    }
+
+    @Provides
+    @Singleton
+    @OptIn(ExperimentalSerializationApi::class)
+    fun provideRetrofit(httpClient: OkHttpClient): Retrofit {
+        val mediaType = MediaType.get("application/json")
+        return Retrofit.Builder()
+            .baseUrl(BASE_URL)
+            .client(httpClient)
+            .addConverterFactory(Json.asConverterFactory(mediaType))
+            .build()
+    }
+}
---
+++ app/src/main/java/com/example/googleauthapp/util/Constants.kt
@@ -0,0 +1,6 @@
+package com.example.googleauthapp.util
+
+object Constants {
+
+    val BASE_URL = "http://10.0.2.2:8080"
+}
