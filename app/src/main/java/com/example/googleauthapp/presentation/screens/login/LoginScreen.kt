970a415ece10571ed0378b31d2c1dd82d6bb8536
Daniil <porvipop@mail.ru>
2022-05-07 17:45:37 +0300

    Create LoginViewModel

--- app/src/main/java/com/example/googleauthapp/presentation/screens/login/LoginScreen.kt
+++ app/src/main/java/com/example/googleauthapp/presentation/screens/login/LoginScreen.kt
@@ -3,20 +3,32 @@ package com.example.googleauthapp.presentation.screens.login
 import androidx.compose.material.Scaffold
 import androidx.compose.material.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavHostController
 import com.example.googleauthapp.domain.model.MessageBarState
 
 @Composable
-fun LoginScreen(navController: NavHostController) {
+fun LoginScreen(
+    navController: NavHostController,
+    viewModel: LoginViewModel = hiltViewModel()
+) {
+
+    val messageBarState by viewModel.messageState
+    val loginState by viewModel.loginState
+
+
     Scaffold(
         topBar = {
             LoginTopAppBar()
         },
         content = {
             LoginContent(
-                messageBarState = MessageBarState(),
-                loginState = true,
-                onLoginClicked = {  }
+                messageBarState = messageBarState,
+                loginState = loginState,
+                onLoginClicked = {
+                    viewModel.login(true)
+                }
             )
 
         }
---
+++ app/src/main/java/com/example/googleauthapp/presentation/screens/login/LoginViewModel.kt
@@ -0,0 +1,38 @@
+package com.example.googleauthapp.presentation.screens.login
+
+import androidx.compose.runtime.MutableState
+import androidx.compose.runtime.State
+import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.googleauthapp.domain.model.MessageBarState
+import com.example.googleauthapp.domain.repository.Repository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class LoginViewModel @Inject constructor(private val repository: Repository) : ViewModel() {
+
+    private val _loginState: MutableState<Boolean> = mutableStateOf(false)
+    val loginState: State<Boolean> = _loginState
+
+
+    private val _messageState: MutableState<MessageBarState> = mutableStateOf(MessageBarState())
+    val messageState: State<MessageBarState> = _messageState
+
+    init {
+        viewModelScope.launch {
+            repository.readLoginState().collect { loginState ->
+                _loginState.value = loginState
+            }
+        }
+    }
+
+    fun login(loginState: Boolean) {
+        viewModelScope.launch(Dispatchers.IO) {
+            repository.insertLoginState(loginState = loginState)
+        }
+    }
+}
