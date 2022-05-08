7f7316c617df539e499c79deda0a3f485d5e78d9
Daniil <porvipop@mail.ru>
2022-05-08 08:28:09 +0300

    Create SignIn and SingUp fun

--- app/src/main/java/com/example/googleauthapp/util/Constants.kt
+++ app/src/main/java/com/example/googleauthapp/util/Constants.kt
@@ -5,4 +5,5 @@ object Constants {
     const val BASE_URL = "http://10.0.2.2:8080"
     const val LOGIN_PREFERENCE_KEY = "login_key"
     const val DATASTORE_PREFERENCES_FILE_NAME = "data_store_preferences_file"
+    const val SERVER_CLIENT_ID = "547859105521-0nroigi4la41sn3j8m43mbanpbsdigen.apps.googleusercontent.com"
 }
